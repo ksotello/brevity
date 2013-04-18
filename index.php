f1f93bb3b2cd5e5b730ab0da67cc68faa42db719
Kyle Sotello <ksotello@uncc.edu>
2013-04-18 00:16:21 -0400

    added boilerplate features

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
   $aOne = new Action("home", $_GET, $brevCfg);
 ?>
 <!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <html>
   <head>
     <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
@@ -19,8 +23,18 @@
     <?PHP echo $aOne->cClass->getCSS();//print $aOne->cClass->getCSS(); ?>
   </head>
   <body>
+	<!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
     <?PHP
       $aOne->Act();
     ?>
+	<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+		var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+        function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src='//www.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
   </body>
 </html>
