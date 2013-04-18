6169a7e0a1a7637d71aa8fc94ef2ca35202226f2
Kyle Sotello <ksotello@uncc.edu>
2013-04-17 23:52:27 -0400

    fixed javascript functionality

---
+++ scripts.php
@@ -0,0 +1,23 @@
+<?PHP
+class Scripts
+{
+	protected $script_a;
+	function __construct()
+    {
+		print 'now i am in the scripts class<br>';
+		$this->script_a = array("jquery.smooth-scroll.min.js",
+						  "jquery-1.7.2.min.js",
+						  "jquery-1.9.1.min.js",
+						  "jquery-ui-1.8.18.custom.min.js",
+						  "lightbox.js"
+						 );
+    }
+
+    function __destruct() { }
+	
+	function getScripts()
+	{
+		return $this->script_a;
+	}
+}
+?>
