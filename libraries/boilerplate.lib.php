740c226c47559da7b641e1605d002f446b088681
ksotello <ksotello@ksotello-Inspiron-1525.(none)>
2013-04-18 12:29:02 -0400

    changed boilerplate and analytics

---
+++ libraries/boilerplate.lib.php
@@ -0,0 +1,23 @@
+<?PHP
+class Boilerplate
+{
+	function __construct($active)
+	{
+		if($active)
+		{	
+			echo '
+			     <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->	
+                             ';
+		}
+		else
+		{
+			echo '';
+		}	
+	}
+
+	function __destruct(){}
+}
+?>
