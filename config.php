b9dd72177be57c0c965530ceed918078928466df
Kyle Sotello <opticalvoid@gmail.com>
2013-04-25 11:32:41 -0300

    Update config.php

--- config.php
+++ config.php
@@ -8,8 +8,12 @@ $brevCfg['theme_path'] = $brevCfg['base_path'].'themes/';
 $brevCfg['script_path'] = $brevCfg['base_path'].'scripts/';
 
 $brevCfg['page_ext'] = '.page.php';
+
+// If you wish to change the title name for your site
+// please do so here
 $brevCfg['title'] = 'Sample Site :: ';
 
+// Add your meta information here for your webpage
 $brevCfg['meta_tags']['author'] = '';
 $brevCfg['meta_tags']['keywords'] = '';
 $brevCfg['meta_tags']['description'] = '';
@@ -28,4 +32,16 @@ $brevCfg['meta_tags']['robots'] = 'none';
 // the examples given in page.class.php
 
 // $brevCfg['jquery'] = 'jquery-1.9.1.min.js';
+
+// For development puporses brevity defaults to true
+ $brevCfg['development'] = true;
+ 
+// For production purposes brevity defaults to false
+// please change this once your in a production setting
+ $brevCfg['production'] = false;
+ 
+// Production routes for brevity. To add more to your
+// site please do so following the example below
+ $brevCfg['routes']['home'] = 'home';
+ 
 ?>
